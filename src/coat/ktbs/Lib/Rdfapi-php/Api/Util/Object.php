6475056bf634232c6a21fbe04b98fa6aaedaad5d
fatma <fatma@fatma-VirtualBox.(none)>
2013-12-02 18:27:12 +0100

    commit

---
+++ src/coat/ktbs/Lib/Rdfapi-php/Api/Util/Object.php
@@ -0,0 +1,38 @@
+<?php
+
+// ----------------------------------------------------------------------------------
+// Class: Object
+// ----------------------------------------------------------------------------------
+
+/**
+ * An abstract object.
+ * Root object with some general methods, that should be overloaded. 
+ * 
+ *
+ * @version  $Id: Object.php 295 2006-06-23 06:45:53Z tgauss $
+ * @author Chris Bizer <chris@bizer.de>
+ *
+ * @abstract
+ * @package utility
+ *
+ **/
+namespace Coat\ktbs\Lib\Rdfapi-php\Api\Util;
+ class Object {
+
+  /**
+   * Serializes a object into a string
+   *
+   * @access	public
+   * @return	string		
+   */    
+	function toString() {
+    	$objectvars = get_object_vars($this);
+		foreach($objectvars as $key => $value) 
+			$content .= $key ."='". $value. "'; ";
+		return "Instance of " . get_class($this) ."; Properties: ". $content;
+	}
+
+ }
+
+
+?>
