0079aaecb787c8e03d4dca2cbec80f4e15d8155a
fatma derbel <derbelfatma@gmail.com>
2014-04-25 09:22:04 +0200

    Update KtbsConfig.php

--- src/Coat/Ktbs/KtbsConfig.php
+++ src/Coat/Ktbs/KtbsConfig.php
@@ -18,7 +18,7 @@ class KtbsConfig
 
     function __construct ()
     {
-        $this->root = "http://http://ktbs.univ-lyon1.fr/" ;
+        $this->root = "http://ktbs.univ-lyon1.fr/" ;
       //  $this->model= "model1";
         $root = new KtbsRoot($this->root);
         if ( $root->exist() ) {$this->exist= true;}
